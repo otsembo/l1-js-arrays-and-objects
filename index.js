37b8fdb19b01654fea284e62d3f6ae349754bc46
Ian Okumu <okumu.otsembo@gmail.com>
2022-06-09 08:37:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,130 @@
+// Utility
+function print(value){
+    console.info(value)
+}
+
+// 1. ARRAYS
+// TODO: Complete the function. Create an array that contains numbers
+function scores (){
+    // create array with 10 elements
+
+    // return the array
+
+}
+// display value
+// print(`Scores Array: ${scores()}`)
+
+
+
+// TODO: Access an element from the array
+function accessScore(){
+    // using array above access the 3rd element
+
+    // return the elememt
+}
+// display value
+// print(`Third Score: ${accessScore()}`)
+
+
+// TODO: Create an array that has multiple nested arrays
+function nestedScores(){
+    // create an array with 3 elements. (The first two elements should be arrays of 2 elements each)
+
+    // return the nested arrays
+}
+// display value
+// print(`Nested Score Array: ${nestedScores()}`)
+
+
+// TODO: Some array methods (length)
+function getNumberOfScores(){
+    // using either of the two arrays created above get the length of the array
+
+    // return the number of elements
+
+}
+// display value
+// print(`Number of elements: ${getNumberOfScores()}`)
+
+
+// TODO: Some array methods
+function addElement(){
+    // using either of the two arrays created above, add two new elements of your choice
+
+    // return the new array with the elements that have been added
+
+}
+// display value
+// print(`Added elements: ${addElement()}`)
+
+
+
+// 2. OBJECTS
+// TODO: Create an student object with the follwoing properties [name, age, email, studentNumber, isKenyan]
+function student(){
+    // create student object
+
+    // return student object
+}
+// display value
+// print(`Student: ${student()}`)
+
+// TODO: Access student's age
+function studentAge(){
+    // access student age using key
+
+
+    // return student age
+
+}
+// display value
+// print(`Student Age: ${studentAge()}`)
+
+
+// TODO: Access student's age
+function studentEmail(){
+    // access student email using dot operator
+
+
+    // return student email
+
+}
+// display value
+// print(`Student Email: ${studentEmail()}`)
+
+
+// TODO(nested objects): create user object that contains course object => [User(id, name, course), Course(name, number)]
+function user(){
+    // create user object
+
+
+    // return user object
+
+}
+// display value
+// print(`User: ${user()}`)
+
+
+// TODO: Some object methods (entries)
+function getUserEntries(){
+    // get user entries
+
+
+    // return entries
+
+}
+// display value
+// print(`User Entries: ${getUserEntries()}`)
+
+
+// TODO: Some object methods (keys)
+function getUserKeys(){
+    // get user keys
+
+
+    // return keys
+
+}
+// display value
+// print(`User Keys: ${getUserKeys()}`)
+
